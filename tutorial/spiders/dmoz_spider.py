fb6d297fe176752e84bc72fd7496693390ff8d80
billjyc <jiayicong@vip.qq.com>
2016-12-16 20:27:33 -0800

    add some comments

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -22,18 +22,15 @@ class MemberSpider(Spider):
         sel = Selector(response)
         member_links = sel.xpath('//div[@class="member_h zx3"]')
         for member_link in member_links:
-            #print member_link
+
             member = MemberItem()
             name = member_link.xpath('./div[@class="mh_w1"]/text()').extract()
-            # for t in name:
-            #     print t.encode("utf-8")
-            member['name'] = name[0]
-            #print name
-            member['link'] = member_link.xpath('./div[1]/a/@href').extract()
 
+            member['name'] = name[0]
+            member['link'] = member_link.xpath('./div[1]/a/@href').extract()
             strs = member['link'][0].split('=')
             member['id'] = strs[1]
-            #print member['id']
+
             url = "http://www.snh48.com/" + member['link'][0]
             member['link'] = url
             yield scrapy.Request(url, meta={'item': member}, callback=self.parse_detail)
@@ -43,7 +40,6 @@ class MemberSpider(Spider):
         sel = Selector(response)
 
         member['image_link'] = "http://www.snh48.com/" + sel.xpath('//div[@class="mem_p"]/img/@src').extract()[0]
-        #print member['image_link'].encode("utf-8")
 
         infos = sel.xpath('//div[@class="mem_w"]/ul')
         member['nick_name'] = infos.xpath('./li[2]/text()').extract()[0]
@@ -55,7 +51,7 @@ class MemberSpider(Spider):
         member['batch'] = infos.xpath('./li[16]/text()').extract()[0]
         member['team'] = infos.xpath('./li[20]/text()').extract()[0]
         member['description'] = infos.xpath('./li[28]/text()').extract()
-        # print member['join_time'], ' ', member['height']
-        # print member
+
+        # Here we must return the member Item, or the result would not be passed to pipeline
         return member
 
